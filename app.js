3e7fda92947c8de17bb0da23d7c2a8286e459ef0
Tim Baek <sy2baek@edu.uwaterloo.ca>
2019-01-23 01:54:35 -0500

    finish log-in & redirecting of pages

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError  = require('http-errors');
+const express      = require('express');
+const path         = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser   = require('body-parser');
+const flash        = require('connect-flash');
+const session      = require('express-session');
+const passport     = require('passport');
+const logger       = require('morgan');
+const mongoose     = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+// initiate the app
+const app = express();
+
+// passport config
+require('./config/passport')(passport);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,25 +26,54 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// express session
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// connect flash
+app.use(flash());
+
+// global vars
+app.use((req,res, next) => {
+  res.locals.success_msg = req.flash('sucess_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// configure mongoose
+mongoose.connect('mongodb://localhost/local', { useNewUrlParser: true })
+  .catch(err => console.log(err));
+
+// routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use((req, res, next) => {
+//   next(createError(404));
+// });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use((err, req, res, next) => {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,79 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Tie' });
-});
-
-router.post('/login', (req,res) => {
-  User.findOne({email: req.body.email})
-   .exec()
-   .then(user => {
-      bcrypt.compare(req.body.password, user.password, (err, result) => {
-         if(err) {
-            return res.status(401).json({
-               failed: 'Unauthorized Access'
-            });
-         }
-         if(result) {
-          const JWTToken = jwt.sign(
-            {
-              email: user.email,
-              _id: user._id
-            },
-            'secret',
-            {
-              expiresIn: '2h'
-            }
-          );
-          return res.status(200).json({
-            success: 'Welcome to the JWT Auth',
-            token: JWTToken
-          });
-         }
-         return res.status(401).json({
-            failed: 'Unauthorized Access'
-         });
-      });
-   })
-   .catch(error => {
-      res.status(500).json({
-         error: error
-      });
-   });;
-});
-
-router.post('/signup', (req, res) => {
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if(err) {
-       return res.status(500).json({
-          error: err
-       });
-    } else {
-      const user = new User({
-        _id: new  mongoose.Types.ObjectId(),
-        email: req.body.email,
-        password: hash    
-      });
-      user.save()
-        .then(result => {
-          console.log(result);
-          res.status(200).json({
-            success: 'New user has been created'
-          });
-        })
-        .catch(error => {
-          res.status(500).json({
-            error: err
-          });
-        });
-    }
-  });
 });
 
 module.exports = router;
