f19f2b203823c325e906cf2fefd73eae9d5ec2de
Jay Park <wngt3319@gmail.com>
2019-01-30 15:40:03 -0500

    Create Game schema + fake game generator

---
+++ models/game.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const faker = require('faker');
+const GameSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true, 
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    thumbnail: {
+        type: String,
+        default: 'http://lorempixel.com/640/480/cats/' 
+    },
+    // htmlSources: [{
+
+    // }],
+    // cssSources: [{
+
+    // }],
+    // jsSources: [{
+
+    // }],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const savedFakeGames = [];
+
+GameSchema.statics.generateFakeGames = function (n = 50) {
+    const fakeGames = [];
+    for (let i = 0; i < n; i++) {
+        fakeGames.push({
+            title: faker.commerce.productName(),
+            creator: faker.internet.userName(),
+            thumbnail: faker.image.abstract(),
+        });
+    }
+    this.insertMany(fakeGames)
+        .then(bulkGames => {
+            bulkGames.forEach((user) => savedFakeGames.push(user.id));
+            // savedFakeGames.push(...bulkGames.insertedIds);
+        })
+        .catch(err => console.log(err));
+}
+
+GameSchema.statics.clearFakeGames = function () {
+    let id;
+    while (savedFakeGames.length !== 0) {
+        id = savedFakeGames.pop();
+        this.deleteOne({
+            _id: id
+        });
+    }
+}
+
+let Game = null;
+try {
+    Game = mongoose.model('Game', GameSchema);
+} catch (e) {
+    Game = mongoose.model('Game');
+}
+
+module.exports = Game;
