b8ce77e845634429b8218ceb9558fc71242702f1
Jay Park <wngt3319@gmail.com>
2019-01-30 10:36:29 -0500

    Fake user generator

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const faker = require('faker');
 const UserSchema = mongoose.Schema({
   name: {
     type: String,
@@ -19,11 +19,42 @@ const UserSchema = mongoose.Schema({
   }
 });
 
+const savedFakeUsers = [];
+
+UserSchema.statics.generateFakeUsers = function (n = 50) {
+  const fakeUsers = [];
+  for ( let i = 0; i < n; i++ ) {
+    fakeUsers.push({
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    });
+  }
+  this.insertMany(fakeUsers)
+    .then(bulkUsers => {
+      savedFakeUsers.push(...bulkUsers.insertedIds);
+    })
+    .catch(err => console.log(err));
+}
+
+UserSchema.statics.clearFakeUsers = function () {
+  let id;
+  while (savedFakeUsers.length !== 0) {
+    id = savedFakeUsers.pop();
+    this.deleteOne({
+      _id: id
+    });
+  }
+}
+
 let User = null;
 try {
   User = mongoose.model('User', UserSchema);
 } catch(e) {
   User = mongoose.model('User');
 }
+
+// Test
+User.generateFakeUsers(10);
 
 module.exports = User;
