adc4f0a072137495206e844686ea2af86d3bff16
Jay Park <wngt3319@gmail.com>
2019-03-06 13:27:51 -0500

    Set up WebSocket on server

---
+++ routes/ws.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const Session = require('./ws/session');
+const Client = require('./ws/client');
+const router = express.Router();
+
+const sessions = new Map;
+
+function createId(len = 6, chars = 'abcdefghjkmnopqrstvwxyz01234567890') {
+    let id = '';
+    while (len--) {
+        id += chars[Math.random() * chars.length | 0];
+    }
+    return id;
+}
+
+function createClient(conn, id = createId()) {
+    return new Client(conn, id);
+}
+
+function createSession(id = createId()) {
+    if (sessions.has(id)) {
+        throw new Error(`Session ${id} already exists`);
+    }
+
+    const session = new Session(id);
+    console.log('Creating session', session);
+
+    sessions.set(id, session);
+
+    return session;
+}
+
+function getSession(id) {
+    return sessions.get(id);
+}
+
+function broadcastSession(session) {
+    const clients = [...session.clients];
+    clients.forEach(client => {
+        client.send({
+            type: 'session-broadcast',
+            peers: {
+                you: client.id,
+                clients: clients.map(client => {
+                    return {
+                        id: client.id,
+                        state: client.state,
+                    }
+                }),
+            },
+        });
+    });
+}
+
+router.ws('/', (ws, req) => {
+    console.log('Connection established');
+    const client = createClient(conn);
+
+    ws.on('message', msg => {
+        console.log('Message received', msg);
+        const data = JSON.parse(msg);
+
+        if (data.type === 'create-session') {
+            const session = createSession();
+            session.join(client);
+
+            client.state = data.state;
+            client.send({
+                type: 'session-created',
+                id: session.id,
+            });
+        } else if (data.type === 'join-session') {
+            const session = getSession(data.id) || createSession(data.id);
+            session.join(client);
+
+            client.state = data.state;
+            broadcastSession(session);
+        } else if (data.type === 'state-update') {
+            const [key, value] = data.state;
+            client.state[data.fragment][key] = value;
+            client.broadcast(data);
+        }
+
+    });
+
+    ws.on('close', () => {
+        console.log('Connection closed');
+        const session = client.session;
+        if (session) {
+            session.leave(client);
+            if (session.clients.size === 0) {
+                sessions.delete(session.id);
+            }
+        }
+
+        broadcastSession(session);
+
+        console.log(sessions);
+    });
+});
+
+module.exports = router;
