7714fff597094ca024697a8b979952a6bff5d967
Tim Baek <tim.baek@hotmail.com>
2019-03-20 17:16:34 -0400

    fix s3 configuration (#17)

--- services/upload.js
+++ services/upload.js
@@ -12,25 +12,34 @@ aws.config.update({
 
 const s3 = new aws.S3();
 
-// const fileFilter = (req, file, cb) => {
-//   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-//     cb(null, true);
-//   } else {
-//     cb(new Error('Invalid file type, only ZIP is allowed!'), false);
-//   }
-// }
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' 
+  || file.mimetype === 'image/png'
+  || file.mimetype === 'text/html'
+  || file.mimetype === 'text/css'
+  || file.mimetype === 'text/javascript'
+  ) {
+    cb(null, true);
+  } else {
+    cb(new Error('Invalid file type'), false);
+  }
+}
 
 const upload = multer({
-  // fileFilter,
+  fileFilter,
   storage: multerS3({
     acl: 'public-read',
     s3: s3,
     bucket: 'tie-cs-494-demo',
+    contentDisposition: 'inline',
+    contentType: function(req, file, cb) {
+      cb(null, file.mimetype);
+    },
     metadata: function (req, file, cb) {
       cb(null, {fieldName: file.fieldname});
     },
     key: function (req, file, cb) {
-      const foldername = req.body.title + '-' + req.user.id;
+      const foldername = req.body.title.toLowerCase();
       const fullpath = foldername + '/' + file.originalname;
       cb(null, fullpath);
     }
